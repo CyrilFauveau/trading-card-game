2e237f14e97edf9b3ea5045ccc4e8bec9571863b
Cyril Fauveau <fauveaucyril@gmail.com>
2025-03-25 20:26:03 +0100

    :feat: first tests

--- test/Base.test.js
+++ test/Base.test.js
@@ -1,10 +1,23 @@
 const { expect } = require("chai");
-const { setupTCGFixture, loadFixture, time } = require("./TradingCardGame");
+const { setupTCGFixture, loadFixture, time } = require("./Setup.test");
 
 describe("Booster opening", function () {
     let owner, user, tradingCardGame, boosterDelay;
 
     beforeEach(async function () {
         ({ owner, user, tradingCardGame, boosterDelay } = await loadFixture(setupTCGFixture));
+    });
+
+    describe("mint()", function () {
+        it("Should revert if not the owner", async function () {
+            await expect(tradingCardGame.connect(user).mint(user.address, 1, 1)).to.be.revertedWithCustomError(tradingCardGame, "OwnableUnauthorizedAccount");
+        });
+
+        it("Should mint a card", async function () {
+            await tradingCardGame.connect(owner).mint(owner.address, 1, 1);
+            const [ids, amounts] = await tradingCardGame.getCardsByUser(owner.address);
+            expect(ids.length).to.equal(1);
+            expect(amounts[0]).to.equal(1);
+        });
     });
 });
