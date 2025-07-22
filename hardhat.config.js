34d2865b253d13235a01b21832662ace80ba4528
cyril fauveau <fauveaucyril@gmail.com>
2025-07-22 16:56:03 +0200

    :build: deployed on sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,30 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv/config");
+require("hardhat-gas-reporter");
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_ID = process.env.ETHERSCAN_ID || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
   networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
     hardhat: {},
     localhost: {
       url: "http://127.0.0.1:8545/",
     }
+  },
+  gasReporter: {
+    enabled: true,
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_ID,
   }
 };
